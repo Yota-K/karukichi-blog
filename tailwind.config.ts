3930131ba2813e234ccab0ac8070dff847a88d26
Y.K <powdersugar828828@gmail.com>
2024-07-05 01:13:33 +0900

    feat: add theme colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,13 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      headerBlue: {
+        background: '#39394d',
+      },
+      primary: '#7180ea',
+      secondary: '#dbeafe',
+    },
     extend: {},
   },
   plugins: [],
