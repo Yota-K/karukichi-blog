6ad4e2b5dc5dc95a771aa09f52164333a75f1705
Y.K <powdersugar828828@gmail.com>
2024-07-06 00:15:03 +0900

    fix: change story name of header component.

--- app/components/Header/Header.stories.tsx
+++ app/components/Header/Header.stories.tsx
@@ -15,7 +15,7 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Base: Story = {
+export const Default: Story = {
   args: {
     children: (
       <Heading as="h1" size="xl" className="text-white">
