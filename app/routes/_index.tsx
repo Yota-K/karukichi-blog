0811d3a136c91e8ff86e65e2b664ab5b7a44d378
Y.K <powdersugar828828@gmail.com>
2024-07-07 18:38:31 +0900

    feat: setup deploy for Cloudflare.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ export const headers: HeadersFunction = () => {
     // - 7日間経過したら、そこから 360s は、古いキャッシュを参照するが、バックグラウンドでfetchを行い、キャッシュを更新して、サーバから新しいリソースを取得できたら、キャッシュを更新する。
     // - 7日を過ぎると古くなるが、サーバーがエラーでレスポンスを返した場合はさらに1日（86400 秒間）利用できる。
     'Cache-Control':
-      'max-age=604800, stale-while-revalidate=360, stale-if-error=86400',
+      'max-age=0, s-maxage=604800, stale-while-revalidate=360, stale-if-error=86400',
   }
 }
 
