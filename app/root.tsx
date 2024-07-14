0d195becab3321b22b92477fa1025262b505f381
Y.K <powdersugar828828@gmail.com>
2024-07-15 00:19:22 +0900

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -27,27 +27,26 @@ export { loader }
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const data = useLoaderData<typeof loader>()
-  // TODO: リリース時に消す
-  if (!data?.authorized) {
-    return <div>Unauthorized</div>
+  if (data?.authorized) {
+    return (
+      <html lang="ja">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    )
   }
-  // TODO: リリース時に消す
 
-  return (
-    <html lang="ja">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  )
+  // TODO: リリース時に消す
+  return <div>Unauthorized</div>
 }
 
 export default function App() {
