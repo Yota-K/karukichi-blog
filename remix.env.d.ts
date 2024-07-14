d04aabf3e78143fcebe5de83b81db545159749f2
Y.K <powdersugar828828@gmail.com>
2024-07-15 00:44:23 +0900

    feat: set up basic authentication.

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,4 +1,8 @@
 interface Env extends z.infer<typeof serverSchema> {
   CMS_API_KEY: string
   RECAPTCHA_KEY: string
+  // TODO: リリース時に消す
+  BASIC_USER: string
+  BASIC_PASS: string
+  // TODO: リリース時に消す
 }
