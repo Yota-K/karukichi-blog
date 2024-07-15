38ecdc253e6b92ce9d2176eedd407a487f315ac7
Y.K <powdersugar828828@gmail.com>
2024-07-15 14:00:35 +0900

    feat: add top page link.

--- app/features/common/AppHeader/AppHeader.tsx
+++ app/features/common/AppHeader/AppHeader.tsx
@@ -1,12 +1,14 @@
-import { Heading, Header as UiHeader } from '../../../components'
+import { Heading, Link, Header as UiHeader } from '../../../components'
 import { Config } from '../../../config'
 
 export const AppHeader = () => {
   return (
     <UiHeader>
-      <Heading as="h1" size="xl" className="text-white">
-        {Config.siteTitle}
-      </Heading>
+      <Link to="/">
+        <Heading as="h1" size="xl" className="text-white">
+          {Config.siteTitle}
+        </Heading>
+      </Link>
     </UiHeader>
   )
 }
