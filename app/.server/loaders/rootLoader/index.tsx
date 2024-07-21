9fcbcf2f331c11733e04cd0ed88a472cffc5f3f0
Y.K <powdersugar828828@gmail.com>
2024-07-21 23:48:18 +0900

    style: remove unnecessary comment.

--- app/.server/loaders/rootLoader/index.tsx
+++ app/.server/loaders/rootLoader/index.tsx
@@ -5,6 +5,5 @@ import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
 export const rootLoader = async ({ context }: LoaderFunctionArgs) => {
   validateEnv(context.cloudflare.env);
 
-  // TODO: ページ全体に共通するデータの取得処理を実装する
   return null;
 };
