ba35736143438f21de648f5fa908c5d2f215da69
Y.K <powdersugar828828@gmail.com>
2024-09-19 00:52:19 +0900

    feat: add kv to articleDetailLoader.

--- app/.server/loaders/articleDetailLoader/index.ts
+++ app/.server/loaders/articleDetailLoader/index.ts
@@ -17,8 +17,12 @@ export const articleDetailLoader = async ({ params, context }: LoaderFunctionArg
     });
   }
 
-  const { CMS_API_KEY } = context.cloudflare.env;
+  const { CMS_API_KEY, RESPONSE_CACHE_KV } = context.cloudflare.env;
+
+  // TODO: 後でリファクタリングする
+  const cacheKey = `post:${params.contentId}`;
   const { status, content, toc } = await cmsUseCase.findPost(client(CMS_API_KEY), params.contentId);
+  await RESPONSE_CACHE_KV.put(cacheKey, JSON.stringify({ status, content, toc }));
 
   if (status === 404) {
     throw new Response(null, {
