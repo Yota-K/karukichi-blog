a6397cf86fb3020ad1cc54b639a7a9df94809da1
Y.K <powdersugar828828@gmail.com>
2024-09-19 01:08:28 +0900

    feat: if exists cached response, add return cache response process.

--- app/.server/loaders/articleDetailLoader/index.ts
+++ app/.server/loaders/articleDetailLoader/index.ts
@@ -21,15 +21,23 @@ export const articleDetailLoader = async ({ params, context }: LoaderFunctionArg
 
   // TODO: 後でリファクタリングする
   const cacheKey = `post:${params.contentId}`;
-  const { status, content, toc } = await cmsUseCase.findPost(client(CMS_API_KEY), params.contentId);
-  await RESPONSE_CACHE_KV.put(cacheKey, JSON.stringify({ status, content, toc }));
+  // TODO: as使わないようにしたいところ
+  const cachedResponse = (await RESPONSE_CACHE_KV.get(cacheKey)) as FindPostDto | null;
 
-  if (status === 404) {
-    throw new Response(null, {
-      status: 404,
-      statusText: 'Not Found',
-    });
+  if (!cachedResponse) {
+    const { status, content, toc } = await cmsUseCase.findPost(client(CMS_API_KEY), params.contentId);
+
+    if (status === 404) {
+      throw new Response(null, {
+        status: 404,
+        statusText: 'Not Found',
+      });
+    }
+
+    await RESPONSE_CACHE_KV.put(cacheKey, JSON.stringify({ status, content, toc }));
+
+    return json({ status, content, toc });
   }
 
-  return json({ status, content, toc });
+  return json(cachedResponse);
 };
