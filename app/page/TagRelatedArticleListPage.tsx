022e2a5affda673c1f9b7caee9703b148b7fd956
Y.K <powdersugar828828@gmail.com>
2024-09-22 13:15:30 +0900

    feat: show skeleton component to tag page.

--- app/page/TagRelatedArticleListPage.tsx
+++ app/page/TagRelatedArticleListPage.tsx
@@ -1,14 +1,22 @@
-import { Heading, Pagination } from '../components';
+import { Await } from '@remix-run/react';
+import { Suspense } from 'react';
+
+import { Heading, Pagination, Skeleton } from '../components';
 import { PostList } from '../features';
 
-import type { Content } from '../types';
+import type { ContentList } from '../types';
+import type { SerializeFrom } from '@remix-run/cloudflare';
 import type { ComponentProps } from 'react';
 
 type Props = {
-  contents: Content[];
+  contents: Promise<SerializeFrom<ContentList['contents']>>;
   tagName?: string;
   paginateNum: ComponentProps<typeof Pagination>['paginateNum'];
   totalCount: ComponentProps<typeof Pagination>['totalCount'];
+};
+
+const SkkeletonList = () => {
+  return Array.from({ length: 10 }, (_, i) => i + 1).map((e) => <Skeleton key={e} />);
 };
 
 export const TagRelatedArticleListPage = ({ contents, tagName, paginateNum, totalCount }: Props) => {
@@ -25,8 +33,16 @@ export const TagRelatedArticleListPage = ({ contents, tagName, paginateNum, tota
         </div>
       )}
       <section>
-        <PostList contents={contents} />
-        <Pagination paginateNum={paginateNum} totalCount={totalCount} />
+        <Suspense fallback={<SkkeletonList />}>
+          <Await resolve={contents}>
+            {(contents) => (
+              <>
+                <PostList contents={contents} />
+                <Pagination paginateNum={paginateNum} totalCount={totalCount} />
+              </>
+            )}
+          </Await>
+        </Suspense>
       </section>
     </div>
   );
