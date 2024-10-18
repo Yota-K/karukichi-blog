5c7df1e4cb2d493d204e8da31a2cc1cc2da0e36c
Y.K <powdersugar828828@gmail.com>
2024-10-18 21:56:29 +0900

    fixed: Resolve unexpected server error. When executing a GET request.

--- app/routes/api.revalidate-cache.tsx
+++ app/routes/api.revalidate-cache.tsx
@@ -1,3 +1,13 @@
+import { json } from '@remix-run/cloudflare';
+
 import { revalidateCacheAction as action } from '../.server';
 
 export { action };
+
+// apiをGETで実行すると、{ "message": "Unexpected Server Error" } が発生してしまうので、エラーを回避するためにローダーを定義している。
+//
+// actionは、ルートに対して GET 以外のリクエスト (DELETE、PATCH、POST、または PUT) が行われた場合は、ローダーの前にアクションが呼び出される。
+// ref: https://remix.run/docs/en/main/route/action#action
+export const loader = async () => {
+  return json({ message: 'Method Not Allowed' }, { status: 405 });
+};
