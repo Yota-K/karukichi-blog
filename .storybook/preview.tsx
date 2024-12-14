4fcf6e873b2a74867056e93bd5d3bfd6aa7bff82
Y.K <powdersugar828828@gmail.com>
2024-12-14 23:36:19 +0900

    fixed: Fixed storybook.

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router';
+
+import type { Preview } from '@storybook/react';
+import '../app/tailwind.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+export default preview;
