72d82413115212dc6c9962005a88108e71b589ce
Y.K <powdersugar828828@gmail.com>
2024-12-14 15:06:46 +0900

    fixed: remove console.

--- app/.server/loaders/rootLoader/index.tsx
+++ app/.server/loaders/rootLoader/index.tsx
@@ -5,7 +5,6 @@ import type { LoaderFunctionArgs } from 'react-router';
 type LoaderResponse = Promise<null>;
 
 export const rootLoader = async ({ context }: LoaderFunctionArgs): LoaderResponse => {
-  console.log(context);
   validateEnv(context.cloudflare.env);
 
   return null;
